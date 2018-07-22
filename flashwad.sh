e6ac9d8872404bffa79e4a73f8c6adf93c1a8f74
OtherCrashOverride <OtherCrashOverride@users.noreply.github.com>
2018-07-22 12:36:09 -0500

    Revert "Changes for device firmware support."
    This reverts commit 71dd56d64416cc38f09785b30aaf7137f53fba6b.

--- flashwad.sh
+++ flashwad.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . ${IDF_PATH}/add_path.sh
-esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud $((921600/2)) --before default_reset --after hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size detect 0x300000 doom1-cut.wad
+esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud $((921600/2)) --before default_reset --after hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size detect 0x100000 "$1"
