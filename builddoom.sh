c8a4dce6c552ab76e945040f1df95dc06d58c92c
Adrian Popa <adrian.popa.gh@gmail.com>
2018-08-27 13:53:38 +0300

    Prepare for flashing any wad

--- builddoom.sh
+++ builddoom.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 make
-../odroid-go-firmware/tools/mkfw/mkfw "Doom (Cheats)" tile.raw 0 16 1048576 app build/esp32-doom.bin 66 6 4194304 wad doom1-cut.wad
+release=`date +%Y%m%d`;
+../odroid-go-firmware/tools/mkfw/mkfw "Doom ($release)" tile.raw 0 16 1048576 app build/esp32-doom.bin 66 6 4198400 wad doom1-cut.wad
 mv firmware.fw Doom.fw
--- flashwad.sh
+++ flashwad.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 . ${IDF_PATH}/add_path.sh
-esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud $((921600/2)) --before default_reset --after hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size detect 0x100000 "$1"
+FILESIZE=$(stat -c%s "$1")
+#adjust filesize to a 4k multiple
+SIZE=`echo "($FILESIZE + 4096)/4096*4096" | bc`;
+esptool.py --chip esp32 --port "/dev/ttyUSB0" --before default_reset --after hard_reset erase_region 0x200000 $SIZE
+esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud $((921600/2)) --before default_reset --after hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size detect 0x200000 "$1"
